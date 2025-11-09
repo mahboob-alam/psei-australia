3bab05c28c87b9851a725db0ca69c59070d9092a
mahboob-alam <mahboob-alam@users.noreply.github.com>
2025-11-09 14:21:23 +1100

    fix: update Cloudflare build script to install Flutter in home directory

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -4,12 +4,19 @@ set -e
 # Install Flutter if not already available
 if ! command -v flutter &> /dev/null; then
     echo "Installing Flutter..."
-    git clone https://github.com/flutter/flutter.git -b stable --depth 1 /opt/flutter
-    export PATH="/opt/flutter/bin:$PATH"
+    cd $HOME
+    git clone https://github.com/flutter/flutter.git -b stable --depth 1
+    export PATH="$HOME/flutter/bin:$PATH"
+    cd -
+else
+    echo "Flutter already installed"
 fi
 
 # Get Flutter version
 flutter --version
+
+# Enable web support
+flutter config --enable-web
 
 # Get dependencies
 flutter pub get
